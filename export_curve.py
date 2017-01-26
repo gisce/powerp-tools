ec5d9e8240b475b65c96ff48fbbe2376bdb140af
Eduard Carreras <ecarreras@gisce.net>
2017-01-26 18:41:41 +0100

    Script to export curves CCH_VAL

--- export_curve.py
+++ export_curve.py
@@ -7,28 +7,32 @@ import click
 from erppeek import Client
 
 
-logger = logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('Export CCH')
 
 
 @click.command()
 @click.option('-s', '--server', help='PowERP Server Uri')
 @click.option('-d', '--days-ago', type=click.INT, help='Number of days ago')
 def main(server, days_ago):
-    uri = urlparse(server)
+    url = urlparse(server)
     server = '{p.scheme}://{p.hostname}:{p.port}'.format(p=url)
     db = url.path.lstrip('/')
     user = url.username
     password = url.password
     c = Client(server, db, user, password)
     logger.info('Connected to %s@%s', server, db)
-    date = (date.today() - timedelta(days=days_ago)).strftime('%Y-%m-%d')
+    d = (date.today() - timedelta(days=days_ago)).strftime('%Y-%m-%d')
 
     export_p5d = c.model('wizard.export.comer.p5d')
-    wiz_id = export_p5d.create({
-        'date_from': date,
-        'date_to': date
+    wiz = export_p5d.create({
+        'date_from': d,
+        'date_to': d,
         'file_format': 'cch_val'
     })
-    logger.info('Generating P5D for %s', date)
-    export_p5d.export([wiz_id])
-    logger.info('P5D exported')
+    wiz.export()
+    logger.info('Queued P5D export for %s', d)
+
+
+if __name__ == '__main__':
+    main()
